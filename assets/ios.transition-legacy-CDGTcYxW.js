11bef4662f866d7190d265afcaa4d66aec77a0f2
Hazeljoy08 <hazeljoycollantes@gmail.com>
2024-05-10 09:21:30 +0800

    Updates

--- assets/ios.transition-legacy-CDGTcYxW.js
+++ assets/ios.transition-legacy-CDGTcYxW.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dzq2p0WA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-cowx6B6s.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
